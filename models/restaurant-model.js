ed36ba054ac2146009400d82cf379cf8136c812f
ryjonespdx <ryjones@pdx.edu>
2020-07-31 17:42:41 -0700

    Completed user and restaurant models (for now)

---
+++ models/restaurant-model.js
@@ -0,0 +1,29 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var RestaurantSchema = new Schema({
+  owner: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  name: { type: String, required: true, maxlength: 100 },
+  cuisine: { type: String, required: true, maxlength: 100 },
+  street: { type: String, required: true, maxlength: 100 },
+  city: { type: String, required: true, maxlength: 100 },
+  state: { type: String, required: true, maxlength: 100 },
+  zip: { type: Number, required: true, maxlength: 20 },
+  number: { type: String, required: true, maxlength: 20 },
+});
+
+// Virtual for restaurant's full address
+RestaurantSchema.virtual("address").get(function () {
+  return `${street} ${city}, ${state} ${zip}`;
+});
+
+// Virtual for restaurant's URL (matches Yelp's style)
+// Example: https://www.yelp.com/biz/toshis-beaverton
+RestaurantSchema.virtual("url").get(function () {
+  var formattedURL = `/${name} ${city}`.toLowerCase();
+  // RegEx from https://stackoverflow.com/questions/2657433/replace-space-with-dash-javascript/2657438
+  return formattedURL.replace(/\s/g, "-");
+});
+
+module.exports = mongoose.model("Restaurant", RestaurantSchema);
--- models/user-model.js
+++ models/user-model.js
@@ -3,10 +3,9 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var UserModelSchema = new Schema({
-  id: Number,
-  email: String,
-  password: String,
-  url: String,
+  username: { type: String, required: true, maxlength: 100 },
+  email: { type: String, required: true, maxlength: 100 },
+  password: { type: String, required: true, maxlength: 100 },
 });
 
 module.exports = mongoose.model("UserModel", UserModelSchema);
