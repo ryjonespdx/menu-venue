642bad167a714af5b38db465dc04a2c237be47ef
ryjonespdx <ryjones@pdx.edu>
2020-08-03 19:26:11 -0700

    Added an environment variable for development and working on integrating Passport.js

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # Menu Venue
 
-A web application for smaller and local business to upload their menus in a quick, clean format
+A web application for restaurants to upload their menus in a quick, clean format.
 
 # Setup
-
-From: https://codeburst.io/build-a-weather-website-in-30-minutes-with-node-js-express-openweather-a317f904897b
 
 1. Install Node.js from https://nodejs.org/en/
 2. In your working directory, run `npm install`
@@ -15,3 +13,11 @@ From: https://codeburst.io/build-a-weather-website-in-30-minutes-with-node-js-ex
 To run the server, run `node app.js`
 
 To run the server with nodemon, run `DEBUG=menu-venue:* npm run devstart`
+
+# Tutorials Used
+
+https://codeburst.io/build-a-weather-website-in-30-minutes-with-node-js-express-openweather-a317f904897b
+
+https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/Tutorial_local_library_website
+
+https://www.freecodecamp.org/news/learn-how-to-handle-authentication-with-node-using-passport-js-4a56ed18e81e/
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,18 +1,23 @@
 /********************************************************************************************
- * indexController: functions to handle GET and POST requests called from index 
+ * indexController: functions to handle GET and POST requests called from index
  * *****************************************************************************************/
 
-const { users, restaurants, menus, menuItems } = require('../mockData');
+// (!) Note: add the following line below all models:
+//     require('../config/passport');
 
-exports.index_get = function(req, res) {
-    res.render('index', { title: 'Menu Venue' });
-}
+const { users, restaurants, menus, menuItems } = require("../mockData");
 
-exports.index_post = function(req, res) {
+exports.index_get = function (req, res) {
+  res.render("index", { title: "Menu Venue" });
+};
 
-    // search the database for...
-    let searchedName = req.body.name;
-    let searchedAddress = req.body.address;
+exports.index_post = function (req, res) {
+  // search the database for...
+  let searchedName = req.body.name;
+  let searchedAddress = req.body.address;
 
-    res.render('restaurant_list', { title: `Results for ${searchedName}`, restaurant_list: [ restaurants[0], restaurants[1] ] });
-}
+  res.render("restaurant_list", {
+    title: `Results for ${searchedName}`,
+    restaurant_list: [restaurants[0], restaurants[1]],
+  });
+};
