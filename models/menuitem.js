e0f132c77e6d78461fb0384e515447c6fe44def6
bbeyene <bbeyene@gmail.com>
2020-08-05 19:32:42 -0700

    changed menu models from list to relationship

--- models/menuitem.js
+++ models/menuitem.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const MenuItemSchema = new Schema({
+  menu: { type: Schema.Types.ObjectId, ref: "Menu", required: true }, // menu it belongs on
   name: { type: String, maxlength: 100, required: true },
   description: { type: String, maxlength: 100 },
   price: { type: Number, required: true }, // cents
